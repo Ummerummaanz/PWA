c9fe8b671077b0549ac3524e96f8c5ec664dfc84
Shankar Ganesan <Shankar.Ganesan@synchronoss.com>
2021-07-08 10:10:39 +0000

    Merge pull request #10 in OASIS/feature-template from husky-upgrade to develop
    Squashed commit of the following:
    
    commit 66d11dd986401ecc534c6176bc1d116f878ba129
    Author: sgan0004 <shankar.ganesan@synchronoss.com>
    Date:   Thu Jul 8 15:32:58 2021 +0530
    
        fix: include tests and mocks as part of linting
    
    commit d81eb691067c797b485b702ddaa841347fa5399c
    Author: sgan0004 <shankar.ganesan@synchronoss.com>
    Date:   Thu Jul 8 13:13:13 2021 +0530
    
        feat: pre-push update to yarn
    
    commit 113e5a3df911e060530e771644577618538dcbbc
    Author: sgan0004 <shankar.ganesan@synchronoss.com>
    Date:   Thu Jul 8 07:58:35 2021 +0530
    
        feat: add commitlint
    
    commit b9595ea6b4effdf31f15cf578a7e3563fc85ca30
    Author: sgan0004 <shankar.ganesan@synchronoss.com>
    Date:   Thu Jul 8 07:32:48 2021 +0530
    
        feat: pre push test
    
    commit e67456c0310c0c192a7b9fa2669cc41b3c61d370
    Author: sgan0004 <shankar.ganesan@synchronoss.com>
    Date:   Thu Jul 8 07:30:41 2021 +0530
    
        feat: test pre-push with exit code during push
    
    commit aa393546df01b080d3c48875b07823289dfc3439
    Author: sgan0004 <shankar.ganesan@synchronoss.com>
    Date:   Wed Jul 7 23:15:31 2021 +0530
    
        feat(feature-template): change npm to yarn
    
    commit 3f363ffb7d202f3ea67341f406aa361a2e82ad03
    Author: sgan0004 <shankar.ganesan@synchronoss.com>
    Date:   Wed Jul 7 23:06:14 2021 +0530
    
        feat(feature-template): husky version and hooks update

---
+++ commitlint.config.js
@@ -0,0 +1,9 @@
+/**
+ * Guidelines: https://github.com/angular/angular/blob/master/CONTRIBUTING.md#type
+ */
+module.exports = {
+  extends: ['@commitlint/config-angular'],
+  rules: {
+    'header-max-length': [2, 'always', 140],
+  },
+};
