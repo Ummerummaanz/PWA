3fb7f1a32d4e5ab82a7b2baabd4308da08a17b96
Syed Kamil Hafeez <shaf0002@Synchronoss.com>
2022-09-29 12:07:16 +0500

    feat: added translation for strings

--- src/components/Feature/index.tsx
+++ src/components/Feature/index.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
+import { Translate } from 'oasis-os-common'
 import { useField } from 'oasis-os-contentful';
+import { useAppState } from 'oasis-os-react';
 import { useEffect } from 'react';
 import { getUserProfile, getUserAvatar, UserAvatarResponse } from 'oasis-feature-api';
 import { ContentfulIcon } from 'oasis-os-theming';
@@ -9,13 +11,14 @@ import Help from '../Help';
 import './style.css';
 
 const Feature: React.FC = () => {
+  const [, , app] = useAppState();
+  const entryId = app?.meta?.contentId as string;
   const defaultAvatar = useField<ContentfulIcon>('avatar');
-  const greetingMessage = useField<string>('greeting');
   const helpIcon = useField<ContentfulIcon>('helpIcon');
   const helpRedirectUrl = useField<string>('helpRedirectUrl');
-
   const [userName, setUserName] = React.useState<string>('');
   const [avatar, setAvatar] = React.useState<UserAvatarResponse>();
+  const greetingMessage = Translate({ id: `${entryId}.greeting` })?.props?.children as string;
 
   useEffect(() => {
     getUserProfile().then((profile) => {
@@ -35,7 +38,7 @@ const Feature: React.FC = () => {
         }
         name={defaultAvatar.fields.name}
       />
-      <Greeting message={`${greetingMessage}${userName}!`} />
+      <Greeting message={`${greetingMessage} ${userName}!`} />
       {helpIcon && (
         <Help
           url={helpIcon.fields.icon.fields.file.url}
---
+++ src/types/VerbiageType.ts
@@ -0,0 +1,3 @@
+export type Verbiage = {
+  greeting: string;
+};
